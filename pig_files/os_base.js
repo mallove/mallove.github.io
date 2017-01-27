50272ee379c2986f93284e07e03f760d8e52ec58
Ethan Mallove <mallove@fas.harvard.edu>
2017-01-27 16:38:05 -0500

    add pig page for testing addthis_toolbox (alternative to addthis_widget)

---
+++ pig_files/os_base.js
@@ -0,0 +1,31 @@
+// Fixes tab behavior after using the skip link in IE or Chrome
+Drupal.behaviors.osBase_skipLinkFocus = {
+	attach : function(ctx) {
+		var $ = jQuery, $skip = $('#skip-link a', ctx);
+
+		$skip.click(function(e) {
+			var target = $skip[0].hash.replace('#', '');
+
+			setTimeout(function() {
+				$('a[name="' + target + '"]').attr('tabindex', -1).focus();
+			}, 100);
+		});
+	}
+};
+
+jQuery(document).ready(function() {
+	jQuery(".cal-export").click(function() {
+		jQuery(".attachment.attachment-before ul").slideToggle();
+		jQuery(".os_events_export_links .last").slideToggle();
+
+	});
+	
+	jQuery(".block-boxes-os_search_solr_search_box").addClass("block-os-search-solr");
+
+    jQuery("figure img").each(function() {
+        var imgwidth = jQuery(this).attr('width');
+        jQuery(this).parent().find('figcaption').css({
+            "width" : + imgwidth
+        });
+    });
+});

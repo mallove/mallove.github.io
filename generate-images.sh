36b3e7b6f6dbcfb87be08eba99b85e310842893a
Ethan Mallove <mallove@fas.harvard.edu>
2017-01-31 13:34:23 -0500

    131-132

--- generate-images.sh
+++ generate-images.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-convert -size 300x300 xc:white -gravity Center -fill cyan -draw 'line 50,50 70,70' -fill red -draw 'line 68,70 72,70 line 70,68 70,72' -fill blue -draw "text 20,20 'og_image 131-126'" text_offset_og_image.jpg
-convert -size 300x300 xc:white -gravity Center -fill cyan -draw 'line 50,50 70,70' -fill red -draw 'line 68,70 72,70 line 70,68 70,72' -fill blue -draw "text 20,20 'twitter_image 131-126'" text_offset_twitter_image.jpg
-convert -size 300x300 xc:white -gravity Center -fill cyan -draw 'line 50,50 70,70' -fill red -draw 'line 68,70 72,70 line 70,68 70,72' -fill blue -draw "text 20,20 'blog_image 131-126'" text_offset_blog_image.jpg
-convert -size 300x300 xc:white -gravity Center -fill cyan -draw 'line 50,50 70,70' -fill red -draw 'line 68,70 72,70 line 70,68 70,72' -fill blue -draw "text 20,20 'data_media 131-126'" text_offset_data_media.jpg
+convert -size 300x300 xc:white -gravity Center -fill cyan -draw 'line 50,50 70,70' -fill red -draw 'line 68,70 72,70 line 70,68 70,72' -fill blue -draw "text 20,20 'og_image 131-132'" text_offset_og_image.jpg
+convert -size 300x300 xc:white -gravity Center -fill cyan -draw 'line 50,50 70,70' -fill red -draw 'line 68,70 72,70 line 70,68 70,72' -fill blue -draw "text 20,20 'twitter_image 131-132'" text_offset_twitter_image.jpg
+convert -size 300x300 xc:white -gravity Center -fill cyan -draw 'line 50,50 70,70' -fill red -draw 'line 68,70 72,70 line 70,68 70,72' -fill blue -draw "text 20,20 'blog_image 131-132'" text_offset_blog_image.jpg
+convert -size 300x300 xc:white -gravity Center -fill cyan -draw 'line 50,50 70,70' -fill red -draw 'line 68,70 72,70 line 70,68 70,72' -fill blue -draw "text 20,20 'data_media 131-132'" text_offset_data_media.jpg
 
 # convert -size 300x300 xc:white -gravity Center -fill cyan -draw 'line 50,50 70,70' -fill red -draw 'line 68,70 72,70 line 70,68 70,72' -fill blue -draw "text 20,20 'Ethan Armstrong'" ethan_armstrong.jpg
